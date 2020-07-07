f68f74ee49dbbb25ac28be11e1d6581ae4c0fa1c
unknown <liran450@gmail.com>
2020-07-07 23:38:07 +0300

    fixed the color buttons

--- script.js
+++ script.js
@@ -16,22 +16,46 @@ function backgroundStyle() {
 	css.textContent = body.style.background + ";";
 }
 
-function setRandomColor() {
-	var arr = [];
-	for (var i=0; i<3;i++) {
-		var num = Math.floor(Math.random() * 256);
-		arr.push(num);
-	}
-		var arrTwo = [];
-	for (var i=0; i<3;i++) {
-		var numTwo = Math.floor(Math.random() * 256);
-		arrTwo.push(numTwo);
-	}
-	var rgbValue = "rgb(" + arr[0] + ", " + arr[1] + ", " + arr[2] + ")";
-	var rgbValueTwo = "rgb(" + arrTwo[0] + ", " + arrTwo[1] + ", " + arrTwo[2] + ")";
-	var newRgb = "linear-gradient(to right, " + rgbValue + ", " + rgbValueTwo + ")";
-	return newRgb;
+// function setRandomColor() {
+// 	var arr = [];
+// 	for (var i=0; i<3;i++) {
+// 		var num = Math.floor(Math.random() * 256);
+// 		arr.push(num);
+// 	}
+//     var rgbValue = "rgb(" + arr[0] + ", " + arr[1] + ", " + arr[2] + ")";
+// 	return rgbValue;
+// }
+
+// function setRandomColor1() {
+//      var arrTwo = [];
+// 	for (var i=0; i<3;i++) {
+// 		var numTwo = Math.floor(Math.random() * 256);
+// 		arrTwo.push(numTwo);
+// 	}
+//     var rgbValueTwo = "rgb(" + arrTwo[0] + ", " + arrTwo[1] + ", " + arrTwo[2] + ")";
+//     return rgbValueTwo
+// }
+function genHexString() {
+    let output = '#';
+    for (let i = 0; i < 6; ++i) {
+        output += (Math.floor(Math.random() * 16)).toString(16);
+    }
+    return output;
 }
+
+function genHexString1() {
+    let output = '#';
+    for (let i = 0; i < 6; ++i) {
+        output += (Math.floor(Math.random() * 16)).toString(16);
+    }
+    return output;
+}
+
+// when we use 16 in toString() - toString(16), it will give us an output of an hexidecimal number.
+// see definition of hexidecimal numbers for more information.
+
+
+
 
 // function generateRandomHexCode() {
 //   var hexCode = "#" 
@@ -44,10 +68,14 @@ function setRandomColor() {
 // }
 
 function getRandomColor() {
-	body.style.background = setRandomColor();
-
+	var first = genHexString()
+	var second = genHexString1()
+	body.style.background = "linear-gradient(to right, " + first + ", " + second + ")";
 	css.textContent = body.style.background + ";";
+	color1.value = first;
+	color2.value = second;
 }
+
 
 window.addEventListener("load", backgroundStyle);
 
